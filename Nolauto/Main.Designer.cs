70eb4b4988766c143ce2c1c2cbb83ea72bd5c8e4
Pippopad <pippopad2017@gmail.com>
2022-12-19 23:11:51 +0100

    Aggiunte le listView per i veicoli e i noleggi

--- Nolauto/Main.Designer.cs
+++ Nolauto/Main.Designer.cs
@@ -35,7 +35,23 @@
             this.salvaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.salvaComeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.esciToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.lstVeicoli = new System.Windows.Forms.ListView();
+            this.colTarga = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.colMarca = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.colTariffa = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.colKilowatt = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.lstNoleggi = new System.Windows.Forms.ListView();
+            this.colId = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.colDataInizio = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.colNumeroGiorni = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.colCosto = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.colCliente = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.colPostiCapacita = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.mainMenu.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
             this.SuspendLayout();
             // 
             // mainMenu
@@ -91,11 +107,119 @@
             this.esciToolStripMenuItem.Text = "&Esci";
             this.esciToolStripMenuItem.Click += new System.EventHandler(this.esciToolStripMenuItem_Click);
             // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.lstVeicoli);
+            this.groupBox1.Location = new System.Drawing.Point(13, 28);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(373, 323);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Veicoli";
+            // 
+            // lstVeicoli
+            // 
+            this.lstVeicoli.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.colTarga,
+            this.colMarca,
+            this.colTariffa,
+            this.colKilowatt,
+            this.colPostiCapacita});
+            this.lstVeicoli.FullRowSelect = true;
+            this.lstVeicoli.GridLines = true;
+            this.lstVeicoli.HideSelection = false;
+            this.lstVeicoli.Location = new System.Drawing.Point(6, 19);
+            this.lstVeicoli.MultiSelect = false;
+            this.lstVeicoli.Name = "lstVeicoli";
+            this.lstVeicoli.Size = new System.Drawing.Size(361, 298);
+            this.lstVeicoli.TabIndex = 0;
+            this.lstVeicoli.UseCompatibleStateImageBehavior = false;
+            this.lstVeicoli.View = System.Windows.Forms.View.Details;
+            // 
+            // colTarga
+            // 
+            this.colTarga.Text = "Targa";
+            // 
+            // colMarca
+            // 
+            this.colMarca.Text = "Marca";
+            this.colMarca.Width = 103;
+            // 
+            // colTariffa
+            // 
+            this.colTariffa.Text = "Tariffa";
+            this.colTariffa.Width = 55;
+            // 
+            // colKilowatt
+            // 
+            this.colKilowatt.Text = "Kilowatt";
+            this.colKilowatt.Width = 50;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.lstNoleggi);
+            this.groupBox2.Location = new System.Drawing.Point(392, 28);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(377, 323);
+            this.groupBox2.TabIndex = 2;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Noleggi";
+            // 
+            // lstNoleggi
+            // 
+            this.lstNoleggi.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.colId,
+            this.colDataInizio,
+            this.colNumeroGiorni,
+            this.colCosto,
+            this.colCliente});
+            this.lstNoleggi.FullRowSelect = true;
+            this.lstNoleggi.GridLines = true;
+            this.lstNoleggi.HideSelection = false;
+            this.lstNoleggi.Location = new System.Drawing.Point(6, 19);
+            this.lstNoleggi.MultiSelect = false;
+            this.lstNoleggi.Name = "lstNoleggi";
+            this.lstNoleggi.Size = new System.Drawing.Size(365, 298);
+            this.lstNoleggi.TabIndex = 0;
+            this.lstNoleggi.UseCompatibleStateImageBehavior = false;
+            this.lstNoleggi.View = System.Windows.Forms.View.Details;
+            // 
+            // colId
+            // 
+            this.colId.Text = "Id";
+            this.colId.Width = 27;
+            // 
+            // colDataInizio
+            // 
+            this.colDataInizio.Text = "Data Inizio";
+            this.colDataInizio.Width = 73;
+            // 
+            // colNumeroGiorni
+            // 
+            this.colNumeroGiorni.Text = "Numero Giorni";
+            this.colNumeroGiorni.Width = 84;
+            // 
+            // colCosto
+            // 
+            this.colCosto.Text = "Costo";
+            // 
+            // colCliente
+            // 
+            this.colCliente.Text = "Cliente";
+            this.colCliente.Width = 116;
+            // 
+            // colPostiCapacita
+            // 
+            this.colPostiCapacita.Text = "Posti/Capacità";
+            this.colPostiCapacita.Width = 87;
+            // 
             // Main
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(781, 363);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.mainMenu);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.MainMenuStrip = this.mainMenu;
@@ -106,6 +230,8 @@
             this.Text = "Nolauto";
             this.mainMenu.ResumeLayout(false);
             this.mainMenu.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -120,6 +246,20 @@
         private System.Windows.Forms.ToolStripMenuItem salvaToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem salvaComeToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem esciToolStripMenuItem;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.ListView lstVeicoli;
+        private System.Windows.Forms.ColumnHeader colTarga;
+        private System.Windows.Forms.ColumnHeader colMarca;
+        private System.Windows.Forms.ColumnHeader colTariffa;
+        private System.Windows.Forms.ColumnHeader colKilowatt;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.ListView lstNoleggi;
+        private System.Windows.Forms.ColumnHeader colId;
+        private System.Windows.Forms.ColumnHeader colDataInizio;
+        private System.Windows.Forms.ColumnHeader colNumeroGiorni;
+        private System.Windows.Forms.ColumnHeader colCosto;
+        private System.Windows.Forms.ColumnHeader colCliente;
+        private System.Windows.Forms.ColumnHeader colPostiCapacita;
     }
 }
 
