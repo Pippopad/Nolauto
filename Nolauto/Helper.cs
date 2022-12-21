7786f7269b9fb54d606da88839d44a34724b198c
Pippopad <pippopad2017@gmail.com>
2022-12-21 09:15:34 +0100

    Aggiunta classe Helper

---
+++ Nolauto/Helper.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Nolauto
+{
+    public static class Helper
+    {
+        public static bool ControlloTarga(string targa)
+        {
+            Regex re = new Regex(@"^[a-zA-Z]{2}[\d]{3}[a-zA-Z]{2}$");
+            return re.Matches(targa).Count != 0;
+        }
+
+        public static void MsgErrore(string messaggio, string titolo)
+        {
+            MessageBox.Show(messaggio, titolo, MessageBoxButtons.OK, MessageBoxIcon.Error);
+        }
+    }
+}
