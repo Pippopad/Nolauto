b9f6cbbed921c4bc3dc9a6d87ed70084a74e47a4
Pippopad <pippopad2017@gmail.com>
2022-12-22 22:56:20 +0100

    Aggiornata la classe dei veicoli

--- Nolauto/Veicolo.cs
+++ Nolauto/Veicolo.cs
@@ -14,10 +14,12 @@ namespace Nolauto
 
     public abstract class Veicolo
     {
-        public string Targa { get; set; }
-        public string Marca { get; set; }
-        public double Tariffa { get; set; }
+        public string Targa { get; }
+        public string Marca { get; }
+        public double Tariffa { get; }
         public double Kilowatt { get; }
+
+        private List<Noleggio> Noleggi;
 
         public Veicolo(string targa, string marca, double tariffa, double kilowatt)
         {
@@ -25,6 +27,42 @@ namespace Nolauto
             this.Marca = marca;
             this.Tariffa = tariffa;
             this.Kilowatt = kilowatt;
+
+            this.Noleggi = new List<Noleggio>();
+        }
+
+        public bool AggiungiNoleggio(Noleggio n)
+        {
+            if (Noleggi.Find((noleggio) => noleggio.Id == n.Id) != null) return false;
+
+            // TODO: Controllo collisione date
+
+            Noleggi.Add(n);
+            return true;
+        }
+
+        public bool RimuoviNoleggio(Noleggio n)
+        {
+            return RimuoviNoleggio(n.Id);
+        }
+
+        public bool RimuoviNoleggio(int id)
+        {
+            Noleggio found = Noleggi.Find((noleggio) => noleggio.Id == id);
+            if (found == null) return false;
+
+            Noleggi.Remove(found);
+            return true;
+        }
+
+        public List<Noleggio> GetAll()
+        {
+            return Noleggi;
+        }
+
+        public Noleggio Get(int id)
+        {
+            return Noleggi.Find((noleggio) => noleggio.Id == id);
         }
     }
 }
